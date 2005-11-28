11778e708e7490d97379d7bd0f810575c82dbb78
Grimur Jonsson <gimmi@idega.com>
2005-11-28 23:36:52 +0000

    possibility to change the url for the weather info

--- src/java/is/idega/block/weather/IWBundleStarter.java
+++ src/java/is/idega/block/weather/IWBundleStarter.java
@@ -12,7 +12,13 @@ package is.idega.block.weather;
 import is.idega.block.weather.business.IcelandicWeatherBusiness;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.ejb.CreateException;
+import javax.ejb.FinderException;
 import com.idega.block.weather.business.WeatherBusiness;
+import com.idega.core.data.ICApplicationBinding;
+import com.idega.core.data.ICApplicationBindingHome;
+import com.idega.data.IDOLookup;
+import com.idega.data.IDOLookupException;
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
 import com.idega.util.EventTimer;
@@ -22,12 +28,44 @@ public class IWBundleStarter implements IWBundleStartable, ActionListener {
 
 	private EventTimer timer;
 	public static final String IW_WEATHER_TIMER = "iw_weather_timer";
-	public static final String URL = "http://beljandi.vedur.is/serthj/askrift/ath?notandi=laddi&lykilord=bakki";
+	private String URL = "http://beljandi.vedur.is/serthj/askrift/ath?notandi=laddi&lykilord=bakki";
 
 	public void start(IWBundle starterBundle) {
+		setURL();
 		timer = new EventTimer(EventTimer.THREAD_SLEEP_24_HOURS/6,IW_WEATHER_TIMER);
 		timer.addActionListener(this);
 		timer.start();
+	}
+
+	/**
+	 * <p>
+	 * Get the url to the weather information. Searches the ICApplicationBinding db table.
+	 * </p>
+	 */
+	private void setURL() {
+		try {
+			String KEY = "ICELANDIC_WEATHER_URL";
+			ICApplicationBindingHome ibHome = (ICApplicationBindingHome) IDOLookup.getHome(ICApplicationBinding.class);
+			try {
+				ICApplicationBinding ab = ibHome.findByPrimaryKey(KEY);
+				URL = ab.getValue();
+			}
+			catch (FinderException e) {
+				try {
+					ICApplicationBinding ab = ibHome.create();
+					ab.setKey(KEY);
+					ab.setValue(URL);
+					ab.setBindingType("weather_block_binding");
+					ab.store();
+				}
+				catch (CreateException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		catch (IDOLookupException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void stop(IWBundle starterBundle) {
