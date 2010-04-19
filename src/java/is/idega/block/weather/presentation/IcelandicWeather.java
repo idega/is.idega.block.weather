1c0b5f601ff15985ebc778267eadb2371922e4e7
Þórhallur Helgason <laddi@idega.com>
2010-04-19 12:37:15 +0000

    Fixing branch mixup

--- src/java/is/idega/block/weather/presentation/IcelandicWeather.java
+++ src/java/is/idega/block/weather/presentation/IcelandicWeather.java
@@ -17,6 +17,7 @@ import com.idega.block.weather.presentation.AbstractWeather;
 public class IcelandicWeather extends AbstractWeather {
 
 	protected static final String IW_ICELANDIC_WEATHER_BUNDLE_IDENTIFIER = "is.idega.block.weather";
+	
 	protected WeatherBusiness getBusiness() {
 		return IcelandicWeatherBusiness.getInstance();
 	}
